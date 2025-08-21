d5cca25972de00cbd89c8a647eb3ee76c36a4f7c
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-08-22 05:56:20 +0700

    update license for simplicity

--- app/Helpers/ImageUploaderHelper.php
+++ app/Helpers/ImageUploaderHelper.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2025 Fahmi Fauzi Rahman
+ * See LICENSE file in the project root for full license information.
+ * 
+ * GitHub: https://github.com/ffrz
+ * Email: fahmifauzirahman@gmail.com
+ */
+
 namespace App\Helpers;
 
 use Intervention\Image\ImageManager;
